052966f315a3d36e48aff1e137a15421d2bbb2e6
Llorens19 <llorenssorianodiego@gmail.com>
2024-09-24 22:48:37 +0200

    Migración del Frontend a nuevo repositorio

---
+++ src/app/core/services/index.ts
@@ -0,0 +1,5 @@
+export * from './category.service';
+export * from './carousel.service';
+export * from './job.service';
+export * from './details-resolver.service';
+export * from './carousel.service';
