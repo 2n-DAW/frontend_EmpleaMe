568531e0175268e706a2e5153e77776a2437a511
Llorens19 <llorenssorianodiego@gmail.com>
2024-09-29 16:53:35 +0200

    Componente slider No funcionando

--- src/app/shared/components/multi-range-slider/multi-range-slider.component.ts
+++ src/app/shared/components/multi-range-slider/multi-range-slider.component.ts
@@ -1,14 +1,13 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SharedModule } from '../../shared.module';
-
+import {MatSliderModule} from '@angular/material/slider';
 
 
 @Component({
   selector: 'app-multi-range-slider',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatSliderModule],
   templateUrl: './multi-range-slider.component.html',
   styleUrls: ['./multi-range-slider.component.css']
 })
@@ -23,54 +22,5 @@ export class MultiRangeSliderComponent {
 
   minthumb: number = 0;
   maxthumb: number = 0;
-
-  onMinPriceChange(event: any): void {
-    const value = parseInt(event.target.value, 10);
-    this.minprice = value;
-    this.mintrigger();
-    this.minpriceChange.emit(this.minprice);
-  }
-
-  onMaxPriceChange(event: any): void {
-    const value = parseInt(event.target.value, 10);
-    this.maxprice = value;
-    this.maxtrigger();
-    this.maxpriceChange.emit(this.maxprice);
-  }
-
-  mintrigger(): void {
-    this.validation();
-    this.minprice = Math.min(this.minprice, this.maxprice );
-    this.minthumb = ((this.minprice - this.min) / (this.max - this.min)) * 100;
-  }
-
-  maxtrigger(): void {
-    this.validation();
-    this.maxprice = Math.max(this.maxprice, this.minprice);
-    this.maxthumb = 100 - (((this.maxprice - this.min) / (this.max - this.min)) * 100);
-  }
-
-  validation(): void {
-    if (/^\d*$/.test(this.minprice.toString())) {
-      if (this.minprice > this.max) {
-        this.minprice = 9500;
-      }
-      if (this.minprice < this.min) {
-        this.minprice = this.min;
-      }
-    } else {
-      this.minprice = 0;
-    }
-    if (/^\d*$/.test(this.maxprice.toString())) {
-      if (this.maxprice > this.max) {
-        this.maxprice = this.max;
-      }
-      if (this.maxprice < this.min) {
-        this.maxprice = 200;
-      }
-    } else {
-      this.maxprice = 10000;
-    }
-  }
 
 }
