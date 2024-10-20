122f431526a63511e85c6205a0357f93a28f6d7c
fmontesdev <f.montesdoria@gmail.com>
2024-10-20 13:53:41 +0200

    Adaptado Frontend para trabajar con varios Backends solo para el login y register

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    api_url: 'http://localhost:3000'
+    api_url1: 'http://localhost:3001',
+    api_url2: 'http://localhost:3002',
+    api_url3: 'http://localhost:3003'
   };
